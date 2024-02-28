c7e6c23841fc5de528fbdd8601828100b91fe59c
jj-vinaysaran <vinayjayjay12@gmail.com>
2024-02-28 09:04:14 +0530

    to admin

--- backend/app.js
+++ backend/app.js
@@ -137,9 +137,9 @@ app.post('/api/employees', async (req, res) => {
 
         const mailOptions = {
           from: 'vinay05saran03@gmail.com',
-          to: 'jjvinaysaran.it2021@citchennai.net',
+          to: adminEmail,
           subject: 'Welcome to the company sector',
-          html: `<p>Hello,</p><p>Welcome to the company sector. Your employee ID is ${employeeId} and your password is ${password}. Kindly login to the website.</p><p>Website Link: <a href="https://emp.azurewebsites.net/login">Login</a></p>`
+          html: `<p>Hello,</p><p>Welcome to the company sector. Your employee ID is ${employeeId} and your password is ${password}. Kindly login to the website.</p><p>Website Link: <a href="https://emp.azurewebsites.net">Login</a></p>`
         };
 
         await transporter.sendMail(mailOptions);
